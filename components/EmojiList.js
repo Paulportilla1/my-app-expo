07d88821fa561d69c8198136071b6aed59dd678e
paulportilla1 <116098987+Paulportilla1@users.noreply.github.com>
2024-01-17 11:05:12 -0500

    cambios2

--- components/EmojiList.js
+++ components/EmojiList.js
@@ -7,8 +7,7 @@ export default function EmojiList({ onSelect, onCloseModal }) {
     require('../assets/images/emoji2.png'),
     require('../assets/images/emoji3.png'),
     require('../assets/images/emoji4.png'),
-    require('../assets/images/emoji5.png'),
-    require('../assets/images/emoji6.webp'),
+    
   ]);
 
   return (
